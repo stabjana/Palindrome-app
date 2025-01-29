80d29fcddb88d33deefba9d92836782eb6dd0d14
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-01-29 15:16:33 +0200

    add styles

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,7 +3,7 @@ import { Typography, Box } from "@mui/material";
 function Header() {
   return (
     <Box sx={{ textAlign: "center", mt: 4, mb: 2 }}>
-      <Typography variant="h4" fontWeight="bold" color="primary">
+      <Typography variant="h4" fontWeight="bold" color="#fff">
         Palindrome Checker
       </Typography>
     </Box>
--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -3,19 +3,36 @@ import {checkPalindrome} from "../PalindromeCheck";
 import { useState } from "react";
 
 export const Input = () => {
-    const [input, setInput] = useState(''); // State für den Input-Wert
+    const [input, setInput] = useState(''); 
     const [isPalindrome, setIsPalindrome] = useState<boolean>(null);
 
-  const handleCheck = () => { // pass input to check function
-      const result = checkPalindrome(input);
+  const handleCheck = () => {
+      const result = checkPalindrome(input); // passing input value to check function
       setIsPalindrome(result);
   };
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", mt: 2 }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        mt: 2,
+        p: 3,
+        backgroundColor: "rgba(0, 0, 0, 0.6)", 
+        borderRadius: 2,
+        boxShadow: "0px 4px 10px rgba(255, 190, 11, 0.5)",
+        maxWidth: 450,
+        mx: "auto",
+      }}
+    >
       
       {isPalindrome !== null && (
-        <Typography sx={{ mt: 2, fontWeight: "bold" }}>
+        <Typography sx={{ 
+            m: 2, fontWeight: "bold",
+            fontStyle: "italic", 
+            color: "#fff"
+            }}>
           {isPalindrome ? "Yep, it's a palindrome!" : "No, it's not a palindrome!"}
         </Typography>
       )}
@@ -25,17 +42,26 @@ export const Input = () => {
         id="input"
         variant="outlined"
         fullWidth
-        sx={{ maxWidth: 400 }}
+        sx={{ maxWidth: 400,
+            bgcolor: "#fff", 
+            input: { color: "#333" }, 
+            label: { color: "#333" } }}
         value={input}
         onChange={(e) => setInput(e.target.value)} 
       />
-      <Button
-      variant="contained"
-      sx={{ mt: 2 }}
-      onClick={handleCheck}
+       <Button
+        variant="contained"
+        sx={{
+          mt: 2,
+          bgcolor: "#3a86ff",
+          color: "#fff",
+          fontWeight: "bold",
+          "&:hover": { bgcolor: "#ff006e" },
+        }}
+        onClick={handleCheck}
       >
         Check
-        </Button>
+      </Button>
     </Box>
   );
 };
