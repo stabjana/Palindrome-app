af1e23650e51dd5bebf5d346899c7fbd3c399223
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-01-29 10:06:25 +0200

    styles: add structure and colors

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -2,14 +2,20 @@ import { TextField, Box, Button } from "@mui/material";
 
 export const Input = () => {
   return (
-    <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
+    <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", mt: 2 }}>
       <TextField
         label="Enter text"
         variant="outlined"
         fullWidth
         sx={{ maxWidth: 400 }}
       />
-      <Button>Check</Button>
+      <Button
+      variant="contained"
+      sx={{ mt: 2 }}
+      //onClick={bla}
+      >
+        Check
+        </Button>
     </Box>
   );
 };
