4d48459b85f7f50c076af017af89127366def0bb
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-01-30 20:06:17 +0200

    refactor checkFunction

--- src/PalindromeCheck.ts
+++ src/PalindromeCheck.ts
@@ -1,5 +1,6 @@
 // split reverse join
 
 export function checkPalindrome(str: string) {
-    return str === str.split('').reverse().join('');
-  }
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, ""); // deletes all other characters and lowerCase
+  return str === str.split("").reverse().join("");
+}
